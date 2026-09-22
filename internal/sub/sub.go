a5d696cf3941da92e7729103ca4928f0e00c98f8
Данила <hl2mp.ru@gmail.com>
2026-09-22 19:28:57 +0300

    fix

--- internal/sub/sub.go
+++ internal/sub/sub.go
@@ -102,8 +102,8 @@ func ShareLinksAll(u model.User, servers []Server) []string {
 	// 2. Кодируем полученные байты в hex-строку
 	result := hex.EncodeToString(hash[:])
 
-	links = append(links, "olcrtc://jitsi?datachannel@https://meet.mamba.group/hl2mpru#"+result+"$#RU Обход списков (RT)")
-	links = append(links, "olcrtc://jitsi?datachannel@https://meet.mamba.group/nodeway#"+result+"$#UK Обход списков (RT)")
+	links = append(links, "olcrtc://jitsi?datachannel@https://meet.egovm.ru/hl2mpru#"+result+"$#RU Обход списков (RT)")
+	links = append(links, "olcrtc://jitsi?datachannel@https://meet.mamba.group/nodeway#"+result+"$#UK Обход списков (MB)")
 
 	links = append(links, "olcrtc://wbstream?vp8channel@hl2mpru#"+result+"$#RU Обход списков (WB)")
 	links = append(links, "olcrtc://wbstream?vp8channel@nodeway#"+result+"$#UK Обход списков (WB)")
